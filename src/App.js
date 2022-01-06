53c068393b33dd1c3ab6d15aa3573f41e56c9be8
gaston alejandro trivi <bastademailscareta@gmail.com>
2022-01-06 02:57:07 -0300

    create react app and basic html structure of components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from './homepage.component';
 
 function App() {
   return (
