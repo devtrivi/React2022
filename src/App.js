d77254726f918fb2543f486083431a01985eca49
devtrivi <gatrivi@gmail.com>
2022-03-27 20:28:04 -0300

    added redux, and implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,60 +1,55 @@
-import React from "react";
-import "./App.css";
-import { Routes, Router, Route } from "react-router-dom";
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
-import SignInAndSignUpPage from "./components/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import React from 'react'
+import { connect } from 'react-redux'
+import { setCurrentUser } from './redux/user/user.actions'
+import './App.css'
+import { Routes, Router, Route } from 'react-router-dom'
+import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
+import SignInAndSignUpPage from './components/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 /* a problem with props (and react) is well illustrated by the history prop. if i want to change it in my menu comp, i will have to make a prop chain from here till there. this is called prop tunneling and as you imagine it does not scale well. */
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null,
-    };
-  }
-  unsubscribeFromAuth = null;
+  unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+    const { setCurrentUser } = this.props
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       //this is good naming of methods
       //this.setState ({ currentUser: user})
       if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+        const userRef = await createUserProfileDocument(userAuth)
 
-        userRef.onSnapshot((snapShot) => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data(),
-            },
-          });
-          console.log(this.state);
-        });
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          })
+        })
       } else {
-        this.setState(
-          { curreentUser: userAuth } /* , ()=>console.log so its async  */
-        );
+        setCurrentUser(userAuth)
       }
-    });
+    })
   }
   componentWillUnmount() {
-    this.unsubscribeFromAuth();
+    this.unsubscribeFromAuth()
   }
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Routes>
-          {" "}
-          <Route path="/" element={<HomePage />} />
-          <Route path="/shop" element={<ShopPage />} />
-          <Route path="/signin" element={<SignInAndSignUpPage />} />
+          {' '}
+          <Route path='/' element={<HomePage />} />
+          <Route path='/shop' element={<ShopPage />} />
+          <Route path='/signin' element={<SignInAndSignUpPage />} />
         </Routes>
       </div>
-    );
+    )
   }
 }
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user)),
+})
+export default connect(null, mapDispatchToProps)(App)
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,67 +1,101 @@
-import React from 'react';
+import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/supreme-archer.svg'
-import styled from 'styled-components';
+import styled from 'styled-components'
 import { auth } from '../../firebase/firebase.utils'
 const Wrapper = styled.div`
-    .header {
-  height: 70px;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 25px;
-
-  .logo-container {
-    height: 100%;
-    width: 70px;
-    padding: 25px;
-  }
-
-  .options {
-    width: 50%;
-    height: 100%;
+  .header {
+    height: 70px;
+    width: 100%;
     display: flex;
-    align-items: center;
-    justify-content: flex-end;
+    justify-content: space-between;
+    margin-bottom: 25px;
 
-    .option {
-      padding: 10px 15px;
+    .logo-container {
+      height: 100%;
+      width: 70px;
+      padding: 25px;
+    }
+
+    .options {
+      width: 50%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+
+      .option {
+        padding: 10px 15px;
+      }
     }
   }
-}
 `
-function Header({currentUser}) {
-    return <div className="header" style={{
+function Header({ currentUser }) {
+  return (
+    <div
+      className='header'
+      style={{
         height: '70px',
         width: '100%',
         display: 'flex',
         justifyContent: 'space - between',
-        marginBottom: '25px'
-    }}>
-        <Link to='/'>
-            <Logo className="logo" style={{
-                height: '100%',
-                width: '70px',
-                padding: '25px'
-            }}></Logo>
+        marginBottom: '25px',
+      }}
+    >
+      <Link to='/'>
+        <Logo
+          className='logo'
+          style={{
+            height: '100%',
+            width: '70px',
+            padding: '25px',
+          }}
+        ></Logo>
+      </Link>
+      <div
+        className='options'
+        style={{
+          width: '50 %',
+          height: '100 %',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+        }}
+      >
+        <Link className='option' style={{ padding: '10px 15px' }} to='/shop'>
+          SHOP
         </Link>
-        <div className="options" style={{
-            width: '50 %',
-            height: '100 %',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'flex-end'
-        }}>
-            <Link className="option" style={{ padding: '10px 15px' }} to='/shop'>SHOP</Link>
-            <Link className="option" style={{ padding: '10px 15px' }} to='/shop'>CONTACT</Link><Link className="option" style={{ padding: '10px 15px' }} to='/signin'>INGRESAR</Link>
-            {
-                currentUser ?
-                <div className='option' style={{ padding: '10px 15px' }} onClick={()=>auth.signOut()}>SIGN OUT</div>
-                :
-                <Link className='option' style={{ padding: '10px 15px' }} to='/signin'>SIGN IN</Link>
-            }
-        </div>
+        <Link className='option' style={{ padding: '10px 15px' }} to='/shop'>
+          CONTACT
+        </Link>
+        <Link className='option' style={{ padding: '10px 15px' }} to='/signin'>
+          INGRESAR
+        </Link>
+        {currentUser ? (
+          <div
+            className='option'
+            style={{ padding: '10px 15px' }}
+            onClick={() => auth.signOut()}
+          >
+            SIGN OUT
+          </div>
+        ) : (
+          <Link
+            className='option'
+            style={{ padding: '10px 15px' }}
+            to='/signin'
+          >
+            SIGN IN
+          </Link>
+        )}
+      </div>
     </div>
+  )
 }
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser,
+})
+
+export default connect(mapStateToProps)(Header)
